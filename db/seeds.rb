4b7381e2685d14b5aa5512584850666073870ec0
James Martin <jmartin2683@gmail.com>
2013-02-03 13:07:44 -0500

    items index; updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.all.map(&:destroy)
+Item.all.map(&:destroy)
+
 User.create(email: 'jmartin2683@gmail.com', first_name: 'James', last_name: 'Martin')
 User.create(email: 'jshih7@gmail.com', first_name: 'Jordan', last_name: 'Shih')
+
+10.times do |t|
+  Item.create(name: "Item #{t}", description: "This is another test item, number #{t}.")
+end
