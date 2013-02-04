b44cffc53dfd95e81f526f4aea73d98d9bef2417
James Martin <jmartin2683@gmail.com>
2013-02-04 01:02:24 -0500

    extract search box into separate view

---
+++ app/assets/javascripts/views/search_box.coffee
@@ -0,0 +1,4 @@
+Prototypable.SearchBox = Ember.TextField.extend
+  classNames: ['search-query'],
+  placeholder: 'Search',
+  action: 'application.search'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :description, :instructions
+  attr_accessible :name, :description, :instructions, :created_at
 end
